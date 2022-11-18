a2869bf85fbbd805aa55281d27b14ebc393276b5
R <abiraj-rajendran@hpe.com>
2022-11-18 09:19:02 +0530

    final deliverables

---
+++ Final Deliverables/Final code/frontend/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import RightPart from './pages/RightPart';
+import { Routes, Route } from 'react-router-dom';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { AllPatientHistory } from './pages/AllPatientHistory';
+import { AddPatient } from './pages/AddPatient';
+import { Landing } from './pages/Landing';
+import { PatientProgress } from './pages/PatientProgress';
+function App() {
+  return (
+    
+    
+<Routes>
+    <Route exact path="/" element={<Landing/>}/>
+    <Route exact path="/landing" element={<Landing/>}/>
+    <Route exact path="/login" element={<Login/>}/>
+    <Route exact path="/register" element={<Register/>}/>
+    <Route exact path="/allpatienthistory" element={<AllPatientHistory/>}/>
+    <Route exact path="/addpatient" element={<AddPatient/>}/>
+    <Route exact path="/patientprogress" element={<PatientProgress/>}/>
+</Routes>
+    
+   
+  );
+}
+
+export default App;
